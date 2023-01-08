dc9feef4af0e10b9b43bb9f99f9f8c6ace451c46
Ella Lukarevskaya <ellalukarevskaq@gmail.com>
2023-01-08 13:04:44 +0200

    added types repository

---
+++ src/main/java/com/example/myGarden/repositories/TypesRepository.java
@@ -0,0 +1,7 @@
+package com.example.myGarden.repositories;
+
+import com.example.myGarden.models.Types;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypesRepository  extends JpaRepository<Types, Long> {
+}
